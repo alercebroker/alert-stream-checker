4bf11869a82b6776f7abdecac89e7e4e919410f5
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-03-04 09:47:10 -0300

    result class

--- utils/result.py
+++ utils/result.py
@@ -1,1 +1,47 @@
+class Result:
+    """Represents the outcome of an operation.
+    Attributes
+    ----------
+    success : bool
+        A flag that is set to True if the operation was successful, False if
+        the operation failed.
+    value : object
+        The result of the operation if successful, value is None if operation
+        failed or if the operation has no return value.
+    error : str
+        Error message detailing why the operation failed, value is None if
+        operation was successful.
+    """
 
+    def __init__(self, success, check_success, value, error):
+        self.success = success
+        self.check_success = check_success
+        self.error = error
+        self.value = value
+
+    @property
+    def failure(self):
+        """True if operation failed, False if successful (read-only)."""
+        return not self.success
+
+    def __str__(self):
+        if self.success:
+            return f"[Success]"
+        else:
+            return f'[Failure] "{self.error}"'
+
+    def __repr__(self):
+        if self.success:
+            return f"<Result success={self.success}>"
+        else:
+            return f'<Result success={self.success}, message="{self.error}">'
+
+    @classmethod
+    def Fail(cls, error):
+        """Create a Result object for a failed operation."""
+        return cls(False, value=None, check_success=False, error=error)
+
+    @classmethod
+    def Ok(cls, value=None, check_success=True):
+        """Create a Result object for a successful operation."""
+        return cls(True, value=value, check_success=check_success, error=None)
